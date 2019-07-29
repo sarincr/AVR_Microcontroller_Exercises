586dfe7b93e4744a1ac3c877afce4ff30507b859
Sarin CR <sarincrg@gmail.com>
2019-07-29 22:29:00 +0530

    Button

--- 02.GPIO_In/01.Button/main.cpp
+++ 02.GPIO_In/01.Button/main.cpp
@@ -2,15 +2,19 @@
 
 int main (void)
 {
-   DDRB = (1<<PB5); // Setting up Fifth Pin of Port B as Output
-   DDRD &= ~(1 << PD2);  // Setting up Second Pin of Port D as Input
+   DDRB = (1<<PB5); // Setting up Fifth Pin of Port B as Output  
+   DDRD &= ~(1 << PD2);  // Setting up Second Pin of Port D as Input. AND will add 0 to that point
 
     while (1)
     {
         if (PIND & (1<<PD2))  // Comparing the status of Pin 2 of Port D with PIND register
+        {
             PORTB |= (1<<PB5);   // Setting up Fifth Pin of Port 5 to Logic High
+        }
         else
+        {
             PORTB &= ~(1<<PB5); // Setting up Fifth Pin of Port 5 to Logic High
+        }
     }
     return 0;
 }
