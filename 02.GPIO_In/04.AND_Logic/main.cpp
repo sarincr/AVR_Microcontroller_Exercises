617700f5b60feda853c1c67fc2430fcfc669d9e5
Sarin CR <sarincrg@gmail.com>
2019-07-29 23:11:01 +0530

    AND

--- 02.GPIO_In/04.AND_Logic/main.cpp
+++ 02.GPIO_In/04.AND_Logic/main.cpp
@@ -8,11 +8,11 @@ int main (void)
 
     DDRB = 0xFF;   // Set all Pins of PORTB as OUTPUT
     DDRD = 0x00; // Set all Pins of PORTD as INPUT
-    int x=0;
+    int x=0,y=0;
     while (1)
     {
       x=PIND & 0x08;
-      x=PIND & 0x10;
+      y=PIND & 0x10;
       if (x==1 && y==1)
       PORTB &= ~0x05;
       if (x==0 && y==0)
