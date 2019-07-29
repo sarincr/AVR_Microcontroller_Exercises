9f55bc887b279ebbb2dd22c83ea289d3e343a39a
Sarin CR <sarincrg@gmail.com>
2019-07-29 23:06:35 +0530

    Bit level

--- 02.GPIO_In/03.Input_to_Var/mains.cpp
+++ 02.GPIO_In/03.Input_to_Var/mains.cpp
@@ -7,16 +7,15 @@ int main (void)
 {
 
     DDRB = 0xFF;   // Set all Pins of PORTB as OUTPUT
-    DDRD &= 0x00; // Set all Pins of PORTD as INPUT
-    PORTD |= 0x04;
+    DDRD = 0x00; // Set all Pins of PORTD as INPUT
     int x=0;
     while (1)
     {
-    	x=PIND & 0x04;
-        if (x) // Checking port status of PORTD wrto 4th Pin of Port D
-            PORTB &= ~0x05;    // Setting up of all the pins of PORT B to Logic High
+      x=PIND & 0x08;
+        if (x) 
+            PORTB &= ~0x08;     
         else
-            PORTB |= 0x05; // Setting up of all the pins of PORT B to Logic Low
+            PORTB |= 0x08;  
     }
     return 0;
 }
